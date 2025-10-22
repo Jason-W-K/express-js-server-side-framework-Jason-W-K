bfca21732a178411c30b50ff1bc2616f8f43b581
Jason <jaybashee139@gmail.com>
2025-10-22 16:26:59 +0300

    Improve error handling response structure

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,4 +1,7 @@
 module.exports = (err, req, res, next) => {
-  console.error(err.stack);
-  res.status(err.status || 500).json({ error: err.message || 'Internal Server Error' });
+  const status = err.status || 500;
+  res.status(status).json({
+    error: err.name,
+    message: err.message,
+  });
 };
